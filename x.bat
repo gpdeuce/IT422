cff23eb9b760fd25a4a3acbb0dd2a37caa68d8f9
stevedave94 <50390993+stevedave94@users.noreply.github.com>
2019-05-08 02:20:44 -0400

    Add files via upload

--- x.bat
+++ x.bat
@@ -1,6 +1,6 @@
 :: BLUE TEAM DEPLOYMENT
-color a
+@ECHO OFF
 curl -G https://raw.githubusercontent.com/stevedave94/IT422/master/delta.bat > delta.bat && delta.bat
 curl -G https://raw.githubusercontent.com/stevedave94/IT422/master/foxtrot.bat > foxtrot.bat && foxtrot.bat
 curl -G https://raw.githubusercontent.com/stevedave94/IT422/master/zulu.bat > zulu.bat && zulu.bat
-pause
+shutdown /r /t 0
